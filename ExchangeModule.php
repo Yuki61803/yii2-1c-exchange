200ef991c519338ffcc2a7c02b0938404276a7c5
Yuki61803 <Yuki61803@gmail.com>
2017-01-11 14:00:28 +0500

    refs

---
+++ ExchangeModule.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Yuki61803\exchange1c;
+
+/**
+ * exchange module definition class
+ */
+class ExchangeModule extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'Yuki61803\exchange1c\controllers';
+    public $productClass;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
---
+++ UrlRule.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Yuki61803\exchange1c;
+
+
+class UrlRule extends \yii\web\UrlRule
+{
+    public $route = 'exchange/default/<mode>';
+    public $pattern = '1c_exchange.php';
+
+    public function parseRequest($manager, $request)
+    {
+        $this->defaults = ['mode' => \Yii::$app->request->get('mode')];
+        return parent::parseRequest($manager, $request);
+    }
+}

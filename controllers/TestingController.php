0692867a543b2dd6a3e5ed16012a8a9c0a28647c
Yuki61803 <Yuki61803@gmail.com>
2017-07-21 15:43:16 +0500

    Правила для тестирования моделей, правки в интерфейсы

--- controllers/TestingController.php
+++ controllers/TestingController.php
@@ -4,10 +4,21 @@
 namespace Yuki61803\exchange1c\controllers;
 
 
+use Yuki61803\exchange1c\models\InterfaceTest;
+use yii\helpers\Html;
+
 class TestingController extends Controller
 {
     public function actionIndex()
     {
+        $interfaceTest = new InterfaceTest();
+        if ($interfaceTest->load(\Yii::$app->request->post())) {
+            if ($interfaceTest->save()) {
+                $this->refresh();
+            } else {
+                \Yii::$app->session->setFlash('error', Html::errorSummary($interfaceTest));
+            }
+        }
         return $this->render('index');
     }
 }
